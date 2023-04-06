51006010fcb9bb7b47817afb344b386784563f4e
luizAlimena96 <luizalimena@outlook.com>
2023-04-06 00:44:42 -0300

    Update weread.py

--- weread.py
+++ weread.py
@@ -1,20 +1,46 @@
-import tkinter as tk 
+import tkinter as tk
+from tkinter import filedialog
 import PyPDF2
 import os
 
+class PDFMerge:
+    def __init__(self):
+        self.pdf_files = []
 
-pdf_files = []
-output_filename = 'merge.pdf'
+        self.root = tk.Tk()
+        self.root.title('WeRead')
 
-for filename in os.listdir('.'):
-    if filename.endswith(""):
-        pdf_files.append(filename)
-pdf_files.sort()
-pdf_writer = PyPDF2.PdfWriter()
+        self.add_button = tk.Button(self.root, text='Add files', command=self.add_button)
+        self.add_button.pack(pady=10)
 
-for filename in pdf_files:
-    pdf_reader = PyPDF2.PdfReader(open(filename, 'rb'))
-    for i in range(pdf_reader.getNumPages()):
-        pdf_writer.addPage(pdf_reader.getPage(i))
-with open(output_filename, 'wb') as f:
-    pdf_writer.write(f)
+
+        self.merge_button = tk.Button(self.root, text='Merge files', command=self.merge_pdf)
+        self.merge_button.pack(pady=10)
+
+        self.status_label = tk.Label(self.root, text='No PDFs added')
+        self.status_label.pack()
+
+        self.root.mainloop()
+    def add_button(self):
+        pdf_files = filedialog.askopenfilenames(filetypes=[('PDF files', '*.pdf')])
+        if pdf_files:
+            self.pdf_files.extend(pdf_files)
+            self.status_label.config(text=f'{len(self.pdf_files)} PDFs added')
+    def merge_pdf(self):
+        if not self.pdf_files:
+            self.status_label.config(text='No PDFs added')
+            return
+        self.pdf_files.sort()
+        pdf_writer = PyPDF2.PdfFileWriter()
+
+        for filename in self.pdf_files:
+            pdf_reader = PyPDF2.PdfReader(open(filename,'rb'))
+            for i in range(pdf_reader.gerNumPages()):
+                pdf_writer.addPage(pdf_reader.getPage(i))
+
+        output_filename = 'merged.pdf'
+        with open(output_filename, 'wb') as f:
+            pdf_writer(f)
+        self.status_label.config(text=f'{len(self.pdf_files)} PDFs merged to {output_filename}')
+if __name__ == "__main__":
+    PDFMerge()
